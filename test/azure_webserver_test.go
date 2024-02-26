aa261330dca1cb7e9a218135abdc367f8dd940e1
Yzhan498 <114200630+Yzhan498@users.noreply.github.com>
2024-02-26 18:55:56 -0500

    Update azure_webserver_test.go prefix_label

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-subscription-id>"
+var subscriptionID string = "45a61a7b-f92a-4559-8168-1d379738daf3"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-username>",
+			"label_prefix": "zhan0865",
 		},
 	}
 
